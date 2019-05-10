9a272b7ca29a7cde684f6ced69c9d255306ad697
lcamarda <camarda.luca@gmail.com>
2019-05-10 15:44:54 -0400

    Delete remove_edge_reservation.ps1

--- remove_edge_reservation.ps1
+++
@@ -1,6 +0,0 @@
-Connect-VIServer vcsa-01a.corp.local -User Administrator@vsphere.local -Password VMware1! | out-null
-$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
-$spec.memoryReservationLockedToMax = $false
-
-(Get-VM -name edge* ).ExtensionData.ReconfigVM_Task($spec)
-get-vm -Name edge* | Get-VMResourceConfiguration | Set-VMResourceConfiguration -MemReservationGB 0
